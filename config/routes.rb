1170a6cba57a32c3cbd405f9963b11bb9e1eb593
unknown <unset>
2020-04-14 23:21:27 +0900

    design improved

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/new'
   get 'posts/:id' => "posts#show"
-  post "posts/create" => "posts/create"
+  post "posts/create" => "posts#create"
 
-  get 'home/top'
+  get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
